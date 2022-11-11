5ab815441b26ef24745136be274591673efc2017
maguibing <kuse012@163.com>
2022-11-11 17:36:06 +0800

    fix

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+
+import Style from './index.module.scss'
+
 const Login = () => {
+	const store = useSelector(state => state)
+
+	useEffect(() => {
+		console.log(store)
+	})
+
 	return (
 		<>
-			<div className="login">
+			<div className={Style.root}>
 				登陆
+				<span>测试</span>
 				<svg className="icon" aria-hidden="true">
 					{/* 使用时，只需要将此处的 iconbtn_like_sel 替换为 icon 的名称即可*/}
 					<use xlinkHref="#iconbtn_like_sel"></use>
